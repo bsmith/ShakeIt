4fda128e8b9215121d5f4d847ed49812bd5548b3
Benjamin Smith <bsmith@cabbage.org.uk>
2023-02-04 22:34:05 +0000

    data: sorted list of ingredients

--- data/make_json.mjs
+++ data/make_json.mjs
@@ -4,12 +4,20 @@ import { makeCocktailsFromRows } from "./src/cocktails.mjs";
 const rows = await readCSVFile('./iba_cocktails.csv');
 console.log(`Read ${rows.length} rows`);
 
-console.log(rows[0]);
-console.log(rows[1]);
-
 const cocktails = makeCocktailsFromRows(rows);
 console.log(`Made ${cocktails.length} cocktails`);
 
 writeJSONFile('./iba_cocktails.json', cocktails);
+
+const ingredientMap = new Map();
+cocktails
+    .flatMap(cocktail => {
+        return cocktail.ingredients.map(ingredient => ingredient.name);
+    })
+    .forEach(ingredientName => {
+        ingredientMap.set(ingredientName, true);
+    });
+const ingredientNames = [...ingredientMap.keys()].sort();
+writeJSONFile('./iba_ingredients.json', ingredientNames);
 
 console.log('*** done ***');
