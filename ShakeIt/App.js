cfa2af023f1cea946b854bd2366ef963b23fe2d5
MikhailGorbunov <68015812+MikhailGorbunov@users.noreply.github.com>
2023-02-06 14:02:20 +0000

    added babel,nativeTailwind installed

--- ShakeIt/App.js
+++ ShakeIt/App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    // <SafeAreaView>
+    <View className="flex-1 items-center justify-center bg-red-600">
+      <Text>Open up App.js to start working on your app!skdfskj</Text>
       <StatusBar style="auto" />
     </View>
+    // </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
