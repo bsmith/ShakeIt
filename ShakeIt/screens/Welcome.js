5d33ed0fab3d05b6b6f16e2276296ba6575a8f38
MikhailGorbunov <68015812+MikhailGorbunov@users.noreply.github.com>
2023-02-06 15:22:03 +0000

    Added heroicons icons and useContent

--- ShakeIt/screens/Welcome.js
+++ ShakeIt/screens/Welcome.js
@@ -3,6 +3,12 @@ import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
+import {
+  UserIcon,
+  ChevronDownIcon,
+  AdjustmentsVerticalIcon,
+  MagnifyingGlassIcon,
+} from "react-native-heroicons/outline";
 
 const Welcome = () => {
   //   const navigation = useNavigation();
@@ -17,6 +23,9 @@ const Welcome = () => {
     <SafeAreaView className="bg-white pt-5">
       <View className="bg-red-400">
         <Text>Open up App.js to start working on your app!skdfskj</Text>
+        <ChevronDownIcon size={20} color="#00CCBB" />
+
+        <MagnifyingGlassIcon size={50} color="#00CCBB" />
         {/* <StatusBar style="auto" /> */}
       </View>
     </SafeAreaView>
