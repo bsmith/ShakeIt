f038be02e326f3d51a8dc957f36e293411f64582
Benjamin Smith <bsmith@cabbage.org.uk>
2023-02-07 15:31:20 +0000

    Show cocktail images

--- ShakeIt/screens/SpecificCocktail.js
+++ ShakeIt/screens/SpecificCocktail.js
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
 
@@ -8,8 +8,18 @@ const SpecificCocktail = ({ route, navigation }) => {
   const { recipeKey } = route.params;
   const recipe = recipeData[recipeKey];
 
+  const img = !recipe.imgUrl.startsWith('http') ? null :
+    <Image
+      source={{
+        uri: recipe.imgUrl,
+        width: 128,
+        height: 128
+      }}
+      />
+
   return (
     <View className="flex flex-column">
+      {img}
       <Text>SpecificCocktail recipeKey={recipeKey}</Text>
       { Object.keys(recipe).map(key => {
         return <Text key={key}>{key} = {JSON.stringify(recipe[key])}</Text>
