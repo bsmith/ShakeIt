d1a3a922ee84bb2c9055ec6bf9332236391146cb
Gintare <gintare.dhawde@gmail.com>
2023-02-12 12:03:35 +0000

    light and dark mode added to shopping list screen

--- ShakeIt/screens/ShoppingList.js
+++ ShakeIt/screens/ShoppingList.js
@@ -8,20 +8,21 @@ import { getDatabase, ref, onValue } from "firebase/database";
 const ShoppingList = () => {
   const navigation = useNavigation();
 
-  const [value, setValue] = useState(null);
-  useEffect(() => {
-    const starCountRef = ref(db, "recipe");
-    // const starCountRef = ref(db, "recipe" + postId + "/starCount");
-    onValue(starCountRef, snapshot => {
-      const data = snapshot.val();
+  // const [value, setValue] = useState(null);
+  // useEffect(() => {
+  //   const starCountRef = ref(db, "recipe");
+  //   // const starCountRef = ref(db, "recipe" + postId + "/starCount");
+  //   onValue(starCountRef, snapshot => {
+  //     const data = snapshot.val();
 
-      setValue(data);
-    });
-  }, []);
+  //     setValue(data);
+  //   });
+  // }, []);
 
     return (
-      <View>
-        <Text>Shopping List</Text>
+      <View className="mx-0 bg-beach-200 flex-1 dark:bg-beach-900 dark:text-white-50">
+  
+        <Text className="my-4 text-4xl text-center font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-cerise-300 ">Shopping List</Text>
       </View>
     );
   };
