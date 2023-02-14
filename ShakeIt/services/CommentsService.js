e22978bde9d973ac30bb91fcba1dcca102b83b10
Gintare <gintare.dhawde@gmail.com>
2023-02-14 13:36:51 +0000

    worked on comment section

--- ShakeIt/services/CommentsService.js
+++ ShakeIt/services/CommentsService.js
@@ -10,17 +10,28 @@ import { db } from "../firebase";
 //     });
 //   }, []);
 
+// // export const getRecipeById = async (recipeId) => {
+//     const resp = await fetch(baseURL + '/recipes/' + recipeId + '.json');
+//     const recipe = await resp.json();
+//     recipe.id = recipeId;
+//     return recipe;
+// };
+
 const exampleComment = {
-    userNickname: "Cocktail Enjoyer",
-    commentText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla imperdiet magna ut eros congue convallis. Donec vestibulum eleifend est, in lacinia ex varius sed. Nam nisl neque, luctus non dapibus.",
-    date: "2023-02-08"
+  userNickname: "Cocktail Enjoyer",
+  commentText:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla imperdiet magna ut eros congue convallis. Donec vestibulum eleifend est, in lacinia ex varius sed. Nam nisl neque, luctus non dapibus.",
+  date: "2023-02-08",
 };
 
 export const getCommentsForRecipe = async (recipeId) => {
-    
-    // return [exampleComment, exampleComment, exampleComment];
-}
+  const resp = await fetch(baseUrl + '/comments/' + recipeId + '.json');
+  const comments = await resp.json();
+  return comments;
+};
 
 export const postComment = async (recipeId, commentText) => {
-    console.log(`postComment: ${recipeId}`, commentText);
-}
+  console.log(`postComment: ${recipeId}`, commentText);
+};
+
+
