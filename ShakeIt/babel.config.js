7bf00b60372afc3983810d1a0c88dc5fd4e1e6ae
Benjamin Smith <bsmith@cabbage.org.uk>
2023-03-10 16:36:02 +0000

    ➖ reanimated is now unused

--- ShakeIt/babel.config.js
+++ ShakeIt/babel.config.js
@@ -5,7 +5,6 @@ module.exports = function (api) {
     plugins: [
       "nativewind/babel",
       '@babel/plugin-proposal-export-namespace-from',
-      'react-native-reanimated/plugin',
       // require('@tailwindcss/forms')
     ],
   };
